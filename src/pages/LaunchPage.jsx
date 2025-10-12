296b89d13f75c73a244e08a87ee09bbc4c4b08b6
Nadeenma <nadeen.makhzomi@gmail.com>
2025-10-12 13:05:57 -0400

    nadeen

--- src/pages/LaunchPage.jsx
+++ src/pages/LaunchPage.jsx
@@ -9,3 +9,27 @@ function LaunchPage() {
 }
 
 export default LaunchPage
+
+<DOCTTYPE html>
+    <html>
+        <head>
+            <title> Launchpage </title>
+            <style>
+                body{
+                    font-family: Dm sans; 
+                    background: a09fcd;
+                }
+
+            button{
+                padding: 10px, 20px;
+                color: #a09fcd;
+                border: none;
+                border-radius: 0px;
+            }
+            </style>
+
+        <body>
+            <button id="profile">Click me!</button>
+        </body>
+        </head>
+    </html>
