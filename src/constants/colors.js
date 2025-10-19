d6ed04bab28850cd5af5e3e7da0811e5ce9766f0
Anna Ambarik <165855749+anna52927@users.noreply.github.com>
2025-10-19 12:07:37 -0400

    work so far

---
+++ src/constants/colors.js
@@ -0,0 +1,10 @@
+export const colors = {
+    background: '#a09fcd',
+    primary: '#caa2be',
+    secondary: '#af9dc4',
+    accent: '#d4a6b0',
+    text: '#000000',
+    lightText: '#8B7355',
+    white: '#FFFFFF',
+    gray: '#A0A0A0'
+};
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,12 +1,29 @@
 import './ProfilePage.css';
-
-
 function ProfilePage() {
 
     return (
-        <div>
-            <header>Profile Page</header>
-        </div>
+        <>
+            <div class="header">
+                <header>
+                    <center>
+                    <button> <a href="./HomePage"> Feed</a></button>
+                    <button> <a href="./HomePage"> DreamScape</a></button>
+                    <button> <a href="./LaunchPage"> Logout</a></button>
+                    </center>
+                </header>
+            </div>
+            
+
+            <div class="body">
+                <center>
+                    <div class = "circle"></div>
+                    <h1> Welcome to your profile! </h1>
+                    <button>Make a Post</button>
+                </center>
+            </div>
+
+            //make post scroll through dreams
+        </>
     )
 }
 
