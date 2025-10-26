fc18ae7c56fd345ca03ddf464a544c80300cd300
Anna Ambarik <165855749+anna52927@users.noreply.github.com>
2025-10-26 12:58:15 -0400

    most recent profile page

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,30 +1,55 @@
-import './ProfilePage.css';
+import "./ProfilePage.css";
 function ProfilePage() {
+  const dreams = [
+    "This is one of my dreams",
+    "This is another one of my dreams",
+    "Link this to the database",
+  ];
 
-    return (
-        <>
-            <div class="header">
-                <header>
-                    <center>
-                    <button> <a href="./HomePage"> Feed</a></button>
-                    <button> <a href="./HomePage"> DreamScape</a></button>
-                    <button> <a href="./LaunchPage"> Logout</a></button>
-                    </center>
-                </header>
+  return (
+    <>
+      <div class="header">
+        <nav>
+          <button>
+            {" "}
+            <a href="./HomePage"> Feed</a>
+          </button>
+          <button>
+            {" "}
+            <a href="./HomePage"> DreamScape</a>
+          </button>
+          <button>
+            {" "}
+            <a href="./LaunchPage"> Logout</a>
+          </button>
+        </nav>
+      </div>
+
+      <div className="profile-container">
+        <img
+          src="public/dreamscape__1_-removebg-preview.png"
+          alt="Profile Picture"
+          className="profile-pic"
+        />
+
+        <h1> Welcome to your DreamScape⭐️</h1>
+        <p className="bio">Below here lies your subconscious</p>
+        <button className="post-btn">
+          <a href="./Post">Make a Post</a>
+        </button>
+      </div>
+      <div className="dream-scroll">
+        <h3 className="dream-title"> Logged Dreams </h3>
+        <div className="scroll-container">
+          {dreams.map((dream, index) => (
+            <div key={index} className="dream-item">
+              {dream}
             </div>
-            
-
-            <div class="body">
-                <center>
-                    <div class = "circle"></div>
-                    <h1> Welcome to your profile! </h1>
-                    <button>Make a Post</button>
-                </center>
-            </div>
-
-            //make post scroll through dreams
-        </>
-    )
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default ProfilePage
+export default ProfilePage;
