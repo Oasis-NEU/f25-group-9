b65aec8a6d9161c40eab143b6ab62533b6d97e1d
Nadeenma <nadeen.makhzomi@gmail.com>
2025-11-04 19:51:00 -0500

    login auth supabase connected

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,21 +1,77 @@
-import './LoginPage.css';
+import "./LoginPage.css";
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { useAuth } from "./useAuth";
+
 function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const { signIn } = useAuth();
+  const navigate = useNavigate();
 
-    return (
-        <>
-            <div class="log">
-            <h1>Login</h1>
-             <label>Username:</label>
-             <input className='inputbox'></input> <div></div>
-             <label>Password:</label> 
-             <input className='inputbox' minlength="8"></input> 
-             <input className="submit" type="submit" value="Sign in" />
-             <br></br>
-             <button>create an account</button>
-        </div>
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setError("");
+    setLoading(true);
 
-        </>
-    )
+    const { error } = await signIn(email, password);
+
+    if (error) {
+      setError(error.message);
+    } else {
+      navigate("/profile");
+    }
+
+    setLoading(false);
+  };
+
+  return (
+    <div className="background">    
+      <div className="log">
+        <h2>Login</h2>
+
+      <form onSubmit={handleLogin}>
+        <label>Email:</label>
+        <input
+          className="inputbox"
+          type="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+        <br></br>
+
+        <label>Password:</label>
+        <input
+          className="inputbox"
+          type="password"
+          minlength="8"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+          <br></br>
+
+        {error && <p className="error">{error}</p>}
+
+        <button className="Login" type="Login">
+          {loading} {loading ? "Logging in..." : "Login"}{" "}
+        </button>
+
+       <br></br>
+
+        <Link to="/createaccount">
+          <button className="Account" type="Account">
+            Create an Account {loading}
+          </button>
+        </Link>
+      </form>
+    </div>
+   </div>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
