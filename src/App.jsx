853774181dc9da9dc2b294a46729db462078d5d8
zoooeeeee <zoepetroianu@gmail.com>
2025-11-09 12:56:59 -0500

    Fixed random urls

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 import './App.css'
 import ProfilePage from './pages/ProfilePage';
 import AnalysisPage from './pages/AnalysisPage';
@@ -7,14 +7,38 @@ import LaunchPage from './pages/LaunchPage';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import CreateAccountPage from './pages/CreateAccountPage';
+import { supabase } from '../supabase.js';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+      setLoading(false);
+    });
+
+    // auth changes (login/logout)
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
 
 
   return (
     <>
       <Router>
         <Routes>
+          <Route 
+            path="*" 
+            element={session ? <Navigate to="/profile" replace /> : <Navigate to="/login" replace />} 
+          />
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="/post" element={<PostPage />} />
           <Route path="/analysis" element={<AnalysisPage />} />
