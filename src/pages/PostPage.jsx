628232bb9c2c8d82e27ce80d0bd536554f1fc1a4
zoooeeeee <zoepetroianu@gmail.com>
2025-11-10 17:17:55 -0500

    Fixed problem where Post css was affecting other pages

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -85,7 +85,7 @@ function PostPage() {
     };
 
     return (
-        <>
+        <div className="post-page-container">
             <button className='back-button' onClick={() => navigate('/profile')}>
                 Back to Profile
             </button>
@@ -155,27 +155,27 @@ function PostPage() {
             </div>
 
             <div id="background-wrap">
-                <div class="x1">
-                    <div class="cloud"></div>
+                <div className="x1">
+                    <div className="cloud"></div>
                 </div>
 
-                <div class="x2">
-                    <div class="cloud"></div>
+                <div className="x2">
+                    <div className="cloud"></div>
                 </div>
 
-                <div class="x3">
-                    <div class="cloud"></div>
+                <div className="x3">
+                    <div className="cloud"></div>
                 </div>
 
-                <div class="x4">
-                    <div class="cloud"></div>
+                <div className="x4">
+                    <div className="cloud"></div>
                 </div>
 
-                <div class="x5">
-                    <div class="cloud"></div>
+                <div className="x5">
+                    <div className="cloud"></div>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
